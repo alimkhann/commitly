ced66cf76125730498ada47988c14ed8f126dfd7
alim <alimkhan.ergebayev@gmail.com>
2025-09-21 01:23:10 +0800

    vercel speed insights and analytics

--- commitly-landing/app/layout.tsx
+++ commitly-landing/app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { ReactNode } from 'react'
 import { GeistSans as GeistSans } from 'geist/font/sans'
+import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export const metadata: Metadata = {
   title: 'Commitly - The AI Code Tutor',
@@ -14,6 +16,8 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="en">
       <body className={`${GeistSans.className} antialiased bg-black text-white`}>
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
