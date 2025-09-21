036221e84144fd0710b64cfd153c3797aa5c9bc5
assanalibatyrkhan <asankek12@gmail.com>
2025-09-21 15:58:31 +0800

    add assan's contact info

--- commitly-landing/app/components/BottomCTA.tsx
+++ commitly-landing/app/components/BottomCTA.tsx
@@ -54,6 +54,8 @@ export default function BottomCTA({
                         <span>{t.copyright}</span>
                         <span>•</span>
                         <a className="underline-offset-2 hover:underline" href="https://github.com/alimkhann" target="_blank">Alimkhan Yergebayev</a>
+                        <span>•</span>
+                        <a className="underline-offset-2 hover:underline" href="https://github.com/asanbl4" target="_blank">Assanali Batyrkhan</a>
                     </div>
                     <nav className="flex items-center gap-6 text-base">
                         <a href="/terms" className="text-[#d9d9d9] hover:underline underline-offset-2">{t.terms}</a>
